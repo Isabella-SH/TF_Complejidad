f23a7e3788cea902282d173e7891c9ea5274f4b3
Isabella-SH <sorianoisabella28@gmail.com>
2023-11-19 18:41:56 -0500

    Aplicar filtros

--- AplicacionInterfaz.py
+++ AplicacionInterfaz.py
@@ -1,3 +1,4 @@
+
 import tkinter as tk
 from tkinter import Toplevel, Label, Button
 from tkinter import font
@@ -16,7 +17,7 @@ import random
 inicio = None
 imagen_grafico = None
 nro_resultados_casas = 0
-
+casas_cercanas= []
 
 def dijkstra(grafo, inicio,comuna,realtor, precio_min, precio_max, dorms_min, baths_min, parking_min):
     distancias = {nodo: float('inf') for nodo in grafo.obtener_nodos()}
@@ -149,47 +150,44 @@ def mostrar_resultados(casas_cercanas, comuna_inicio):
     
     
 
-
-                                                                                                                                                    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 def filtrar_por_atributo(recommendations, filtro):
-    
     # Filtrar según el criterio especificado en el filtro
     if filtro == "dorms":
         # Ordenar casas por el atributo 'dorms'
         sorted_houses = sorted(recommendations, key=lambda x: grafo.nodos[x[0]]['dorms'])
-        casas_ordenadas_por_dorms = [casa for casa, _ in sorted_houses]
+        casas_ordenadas_por_dorms = [(casa, distancia) for casa, distancia in sorted_houses]
         return casas_ordenadas_por_dorms
 
     elif filtro == "baths":
         # Ordenar casas por el atributo 'baths'
         sorted_baths = sorted(recommendations, key=lambda x: grafo.nodos[x[0]]['baths'])
-        casas_ordenadas_por_baths = [casa for casa, _ in sorted_baths]
+        casas_ordenadas_por_baths = [(casa, distancia) for casa, distancia in sorted_baths]
         return casas_ordenadas_por_baths
 
     elif filtro == "parking":
         # Ordenar casas por el atributo 'parking'
-        sorted_park= sorted(recommendations, key=lambda x: grafo.nodos[x[0]]['parking'])
-        casas_ordenadas_por_park = [casa for casa, _ in sorted_park]
+        sorted_park = sorted(recommendations, key=lambda x: grafo.nodos[x[0]]['parking'])
+        casas_ordenadas_por_park = [(casa, distancia) for casa, distancia in sorted_park]
         return casas_ordenadas_por_park
     
     elif filtro == "price_usd":
         # Ordenar casas por el atributo 'price_usd'
-        sorted_price_usd= sorted(recommendations, key=lambda x: grafo.nodos[x[0]]['price_usd'])
-        casas_ordenadas_por_price_usd = [casa for casa, _ in sorted_price_usd]
+        sorted_price_usd = sorted(recommendations, key=lambda x: grafo.nodos[x[0]]['price_usd'])
+        casas_ordenadas_por_price_usd = [(casa, distancia) for casa, distancia in sorted_price_usd]
         return casas_ordenadas_por_price_usd
 
-    elif filtro == "desc": #filtra casas que tengan como descuento true
-        return [casa for casa, distancia in recommendations if grafo.nodos[casa]['desc']]
+    elif filtro == "desc":
+        # Filtrar casas que tengan 'desc' como True
+        return [(casa, distancia) for casa, distancia in recommendations if grafo.nodos[casa]['desc']]
     
     elif filtro == "total_area":
         # Ordenar casas por el atributo 'price_usd'
-        sorted_price_total_area= sorted(recommendations, key=lambda x: grafo.nodos[x[0]]['total_area'])
-        casas_ordenadas_por_total_area= [casa for casa, _ in sorted_price_total_area]
+        sorted_price_total_area = sorted(recommendations, key=lambda x: grafo.nodos[x[0]]['total_area'])
+        casas_ordenadas_por_total_area = [(casa, distancia) for casa, distancia in sorted_price_total_area]
         return casas_ordenadas_por_total_area
 
     elif filtro == "todo":
         return recommendations  # Si el filtro no es reconocido, devolvemos la lista completa
-                                                                                                                                                    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
 # Ejemplo de uso
@@ -320,40 +318,43 @@ root = tk.Tk()
 root.title("Recomendador de Casas")
   
 def get_recommendations():
-     comuna = comuna_var.get()
-     realtor = realtor_var.get()
-     price_min = int(price_min_entry.get())
-     price_max = int(price_max_entry.get())
-     bedrooms = int(bedrooms_var.get())
-     bathrooms = int(bathrooms_var.get())
-     parking = int(parking_var.get())
-    
-     inicio = None
-     for nodo in grafo.nodos:
-         if grafo.nodos[nodo]['comuna'] == comuna:
-             inicio = nodo
-             break
+    comuna = comuna_var.get()
+    realtor = realtor_var.get()
+    price_min = int(price_min_entry.get())
+    price_max = int(price_max_entry.get())
+    bedrooms = int(bedrooms_var.get())
+    bathrooms = int(bathrooms_var.get())
+    parking = int(parking_var.get())
 
-     recommendations = dijkstra(grafo, inicio, comuna, realtor, price_min, price_max, bedrooms, bathrooms, parking)
-    
-     # Elimina todas las filas actuales en el Treeview
-     for item in treeview.get_children():
+    inicio = None
+    for nodo in grafo.nodos:
+        if grafo.nodos[nodo]['comuna'] == comuna:
+            inicio = nodo
+            break
+
+    recommendations = dijkstra(grafo, inicio, comuna, realtor, price_min, price_max, bedrooms, bathrooms, parking)
+
+    # Pass the recommendations and selected attribute to the filtering function
+    filtro_seleccionado = atributo_var.get()
+    casas_filtradas = filtrar_por_atributo(recommendations, filtro_seleccionado)
+
+    # Update the Treeview with the filtered results
+    for item in treeview.get_children():
+        print(treeview.get_children())
         treeview.delete(item)
 
-     for i, (casa, distancia) in enumerate(recommendations):
-         casa_data = [
-             i + 1, 
-             grafo.nodos[casa]['comuna'],
-             grafo.nodos[casa]['realtor'],
-             grafo.nodos[casa]['price_usd'],
-             grafo.nodos[casa]['dorms'],
-             grafo.nodos[casa]['baths'],
-             grafo.nodos[casa]['parking'],
-             grafo.nodos[casa]['total_area'],  
-             grafo.nodos[casa]['desc'],  
-             distancia
-         ]
-         treeview.insert('', 'end', values=casa_data)
+    for i, (casa, distancia) in enumerate(casas_filtradas):
+        casa_data = [
+            i + 1,
+            grafo.nodos[casa]['comuna'],
+            grafo.nodos[casa]['realtor'],
+            grafo.nodos[casa]['price_usd'],
+            grafo.nodos[casa]['dorms'],
+            grafo.nodos[casa]['baths'],
+            grafo.nodos[casa]['parking'],
+            distancia
+        ]
+        treeview.insert('', 'end', values=casa_data)
 
 # Crear el widget Treeview para mostrar las recomendaciones
 columns = ('#', 'Comuna', 'Realtor', 'Precio USD', 'Dormitorios', 'Banos', 'Estacionamientos', 'Distancia')
@@ -374,11 +375,11 @@ parking_var = tk.StringVar()
 
 # Etiquetas y entradas para las opciones del usuario
 tk.Label(root, text="Comuna:").grid(row=0, column=0)
-comuna_entry = ttk.Combobox(root, textvariable=comuna_var, values=["LoBarnechea","Vitacura","PuenteAlto", "LasCondes", "QuintaNormal","PedroAguirreCerda", "EstacionCentral", "Colina","LaFlorida", "SanBernardo","Santiago", "LasCondes", "Lampa", "Quilicura","Santiago","SanBernardo","PadreHurtado","Buin","Recoleta"])
+comuna_entry = ttk.Combobox(root, textvariable=comuna_var, values=["PuenteAlto", "LasCondes", "QuintaNormal","PedroAguirreCerda", "EstacionCentral", "Colina","LaFlorida", "SanBernardo","Santiago", "LasCondes", "Lampa", "Quilicura"])
 comuna_entry.grid(row=0, column=1)
 
 tk.Label(root, text="Realtor:").grid(row=1, column=0)
-realtor_entry = ttk.Combobox(root, textvariable=realtor_var, values=["Viel la Dehesa SPA","Viel Propiedades dos Ltda","Schumacher Propiedades","OSSANDON CORREDORES ASOCIADOS S.A","Easyprop","Lyon y Balmaceda Ltda.","Bofill y Asociados Ltda","Nieto & Stone Propiedades","Invistus Spa","Unne","Hobbins Vitacura","Welink Propiedades SpA","Vidal Riedel Propiedades - Las Condes","Nexxos","Mocahome Corredores Integrados","Manterola Propiedades","Tzani Propiedades","Legale y Propiedades Spa", "Propiedadesrs", "Patricia Gajardo propiedades", "Todo Propiedades","Paula Vivanco Arenas", "Fe Propiedades Spa", "Ecorredores Getion Inmobiliaria","Behouse", "Propital", "Cgs Corretaje Inmobiliario Spa", "Giordano Propiedades"])
+realtor_entry = ttk.Combobox(root, textvariable=realtor_var, values=["","Legale y Propiedades Spa", "Propiedadesrs", "Patricia Gajardo propiedades", "Todo Propiedades","Paula Vivanco Arenas", "Fe Propiedades Spa", "Ecorredores Getion Inmobiliaria","Behouse", "Propital", "Cgs Corretaje Inmobiliario Spa", "Giordano Propiedades"])
 realtor_entry.grid(row=1, column=1)
 
 tk.Label(root, text="Precio Minimo:").grid(row=2, column=0)
@@ -404,7 +405,24 @@ parking_entry.grid(row=6, column=1)
 recomendaciones_btn = tk.Button(root, text="Obtener Recomendaciones", command=get_recommendations)
 recomendaciones_btn.grid(row=7, column=0, columnspan=2, pady=10)
 
-#fachadas_btn = tk.Button(root, text="Mostrar Fachadas", command=mostrar_fachadas)
-#fachadas_btn.grid(row=8, column=0, columnspan=2, pady=10)
+# Crear el widget Treeview para mostrar las recomendaciones
+columns = ('#', 'Comuna', 'Realtor', 'Precio USD', 'Dormitorios', 'Banos', 'Estacionamientos', 'Distancia')
+treeview = ttk.Treeview(root, columns=columns, show='headings')
+
+# Configurar las columnas
+for col in columns:
+    treeview.heading(col, text=col)
+
+treeview.grid(row=9, column=0, columnspan=2)
+
+atributo_var = tk.StringVar()
+atributo_var.set("todo") 
+
+atributo_label = tk.Label(root, text="Filtrar por Atributo:")
+atributo_label.grid(row=10, column=0)
+
+atributo_entry = ttk.Combobox(root, textvariable=atributo_var, values=["todo", "dorms", "baths", "parking", "price_usd", "desc", "total_area"])
+atributo_entry.grid(row=10, column=1)
+
 
 root.main
